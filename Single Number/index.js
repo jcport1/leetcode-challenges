a2204df3bd6aee6ff70b9bee16d6560a75bc4c13
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-05-31 10:34:11 -0400

    Solved with for of and for in loop

--- Single Number/index.js
+++ Single Number/index.js
@@ -1,7 +1,7 @@
 //Given a non-empty array of integers nums, every element appears twice except for one. 
 //Find that single one.
 
-const nums = [2,2,1]
+const nums = [2,2,1,3,3,1,5]
 
 var singleNumber = function(nums) {
   
@@ -15,6 +15,7 @@ const obj = {}; // {100:1, 24: 3, 1543: 8}
 //Iterate over the array and assign into a hash that keeps a tally
 
 for (const num of nums) {
+    // console.log(num)
     obj[num] = obj[num] + 1 || 1;
 }
 
