97be411427b577c3a49e15559cd54803adaa7b96
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-06-29 16:22:49 -0400

    Explains replace method

--- 06 - Counting-Bits/index.js
+++ 06 - Counting-Bits/index.js
@@ -8,6 +8,8 @@ function countBits(n) {
      //loop through every num until length of n is reached
      for (let i = 0; i <= n; i++) {
          //set the binary using regex and replace function to remove 0s
+         //replace method returns a new string w/ some or all matches or a pattern replaced by replacement
+         //patten can ebe a string or a RegExp, and rhe replacement can be a string or a function to be called for each match
          //toString(2)transforms i to binary representation
          let binary = i.toString(2).replace(/0/g, "").length;
          console.log(binary)
