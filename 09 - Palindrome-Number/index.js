f82cb80d4249b38361753aac945068ed27b602a3
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-07-02 18:23:06 -0400

    LeetCode 9 solution

---
+++ 09 - Palindrome-Number/index.js
@@ -0,0 +1,37 @@
+// LeetCode 9. Palindrome Number 
+
+//https://leetcode.com/problems/palindrome-number/
+
+var isPalindrome = function(x) {
+
+    if (x > 2 ** 31 - 1) {
+        return false;
+    }
+    
+    //set a new array called result
+    let result = [];
+    
+    //make x a string
+    const str = x.toString();
+    
+    //iterate through the string
+    for (i = 0; i < str.length; i++){
+        //add element to the front (to make backwards)
+        result.unshift(str[i]);   
+    }
+
+   //convert result array into string for comparison
+
+   const resultStr = result.join("")
+
+    // return true or false
+        if (str === resultStr){
+            return true; 
+        } else {
+
+            return false; 
+        }
+    }
+
+
+isPalindrome(1211);
