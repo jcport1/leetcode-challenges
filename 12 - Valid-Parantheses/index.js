5698316b0c84ff78895818f9655830d91422b1d7
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-07-20 18:44:57 -0400

    LeetCode 20 valid parentheses solution

--- 12 - Valid-Parantheses/index.js
+++ 12 - Valid-Parantheses/index.js
@@ -7,23 +7,45 @@
 // 1. Open brackets must be closed by the same type of brackets
 // 2. Open brackets mjst be closed in the correct order
 
+// function isValid(s) {
+//   //correct characters: '()','[]','{}'
+//   const validCharacters = ["()", "[]", "{}"];
+//   let arr = s.split("");
+//   //loop through the string
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let compareS = arr[i] + arr[i + 1];
+
+//     if (validCharacters.includes(compareS)) {
+//       console.log("match", arr[i], arr[i + 1]);
+//     }
+//   }
+
+//   return true;
+//   //if correct return true
+//   //else return false
+// }
+
 function isValid(s) {
-  //correct characters: '()','[]','{}'
-  const validCharacters = ["()", "[]", "{}"];
-  let arr = s.split("");
-  //loop through the string
+  let bracket = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+  let heap = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    let compareS = arr[i] + arr[i + 1];
-
-    if (validCharacters.includes(compareS)) {
-      console.log("match", arr[i], arr[i + 1]);
+  for (let char of s) {
+    if (bracket[char]) {
+      heap.push(bracket[char]);
+      console.log(bracket[char]);
+    } else {
+      if (heap.pop() != char) return false;
     }
   }
 
-  return true;
-  //if correct return true
-  //else return false
+  console.log(!heap.length);
+
+  return !heap.length;
 }
 
 isValid("()[]{}"); //true
