d5e02b6a5524f765ae83b07b65a457dbbfd4ec90
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-07-22 22:56:48 -0400

    Leetcode 27 alternative solution

--- 13 - Remove-Element/index.js
+++ 13 - Remove-Element/index.js
@@ -1,17 +1,30 @@
 //27. Remove Element:https://leetcode.com/problems/remove-element/
 
+// function removeElement(nums, val) {
+//   for (i = 0; i < nums.length; i++) {
+//     //iterate and check if element = val
+//     if (nums[i] === val) {
+//       console.log(i);
+//       //remove element
+//       nums.splice(i, 1);
+//       //decrement so the next iteration starts at the next index
+//       i--;
+//     }
+//   }
+//   return nums.length;
+// }
+
+//Alternative solution
+
 function removeElement(nums, val) {
-  for (i = 0; i < nums.length; i++) {
-    //iterate and check if element = val
-    if (nums[i] === val) {
-      console.log(i);
-      //remove element
-      nums.splice(i, 1);
-      //decrement so the next iteration starts at the next index
-      i--;
+  let zeroStartIdx = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      zeroStartIdx++;
     }
+    console.log(zeroStartIdx);
   }
-  return nums.length;
+  return zeroStartIdx;
 }
 
-removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2);
+removeElement([0, 1, 2, 2, 3, 0, 4, 2], 3);
